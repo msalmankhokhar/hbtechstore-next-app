322ee67561e5f46462b4ba043aeac7bb7b68c091
msalmankhokhar <kjokhars@gmail.com>
2024-12-18 19:15:02 +0500

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1 className="text-xl font-bold">HB Tech Store</h1>
+      <div>
+        <h2 className="text-lg font-semibold">
+          
+        </h2>
+      </div>
+    </div>
+  );
+}
