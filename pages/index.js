2780e588c8b6ec8f0dff3544c7c29bbba0e12206
msalmankhokhar <kjokhars@gmail.com>
2024-12-18 19:28:52 +0500

    modified:   pages/index.js 	modified:   styles/globals.css

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container">
-      <h1 className="text-xl font-bold">HB Tech Store</h1>
-      <div>
-        <h2 className="text-lg font-semibold">
-          
-        </h2>
+    <div className="container px-5 py-10 mx-auto flex flex-col items-center">
+      <h1 className="text-lg font-bold text-red-500">HB Tech Store</h1>
+      <p className="text-base font-light">(Comming soon)</p>
+      <div className="flex flex-col gap-2 my-3">
+        <Image src={'/img/bonga.jpg'} width={200} height={600} alt="Junaid bonga"/>
+        <div>
+          <h2 className="text-sm text-gray-600">Website ka Malik:</h2>
+          <div>
+            <div className="font-bold text-lg">Mirza Junaid Husnain</div>
+            <div className="font-semibold text-gray-700">{`(urf "Bonga")`}</div>
+          </div>
+
+        </div>
       </div>
     </div>
   );
